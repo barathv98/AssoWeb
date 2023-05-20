21bff2df2bac54e6c6762e8581013c386f192c36
Barathkumar <barath@192.168.1.101>
2023-05-20 17:53:10 +0200

    feat(): changes

---
+++ src/common/components/Sidebar/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { GrClose } from "react-icons/gr";
+import styles from './styles.module.scss';
+
+interface SidebarProps {
+    logo?: boolean;
+    onClose: () => void;
+    content: React.ReactNode;
+}
+const Sidebar: FC<SidebarProps> = (props) => {
+    const { logo = true, onClose, content } = props;
+    return (
+        <div className={styles.sidebar}>
+            <div className={styles.container}>
+                <div className={styles.closeBtn} onClick={onClose}><GrClose /></div>
+                <div className={styles.title}>
+                    {logo && <img className={styles.logo} src={require('../../../assets/images/logo.png')} alt='company logo' />}
+                </div>
+                {content}
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
