39c40f99b15f87edaa5732c34d9776e1749ae989
Barathkumar <barath@192.168.1.101>
2023-05-20 15:08:39 +0200

    changes

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -7,12 +7,12 @@ interface AboutUsProps {
 const AboutUs:FC<AboutUsProps> = ({ aboutRef }) => {
     return (<div className={styles.aboutUs} ref={aboutRef}>
         <div className={styles.image}>
-            <img src={require('../../assets/images/about.jpg')} alt="about us" />
+            <img src={require('../../assets/images/about_us.jpg')} alt="about us" />
         </div>
         <div className={styles.text}>
             <div className={styles.title}>About Us</div>
             <div className={styles.content}>
-                The DVC Group has served society through various ventures for more than 100 years. Peepal Tree, the Educational Wing of DVC Group, was  established in 2015 and is a group of premier foundation educational  institutions that offer quality preschool education, daycare, activity  centres, and after school programmes.
+                
             </div>
         </div>
     </div>)
--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react';
 import styles from './styles.module.scss';
+import { MdArrowForwardIos } from "react-icons/md";
+import {ReactComponent as BookIcon} from '../../assets/icons/book.svg';
+import {ReactComponent as DiaryIcon} from '../../assets/icons/diary.svg';
+import {ReactComponent as RegisterIcon} from '../../assets/icons/register.svg';
 
 interface ProductsProps {
     productRef: any;
@@ -9,9 +13,50 @@ const Products: FC<ProductsProps> = ({ productRef }) => {
         <div className={styles.image}>
             <img src={require('../../assets/images/about.jpg')} alt="about us" />
         </div>
-        <div className={styles.text}>
-            <div className={styles.title}>Products</div>
-            The DVC Group has served society through various ventures for more than 100 years. Peepal Tree, the Educational Wing of DVC Group, was  established in 2015 and is a group of premier foundation educational  institutions that offer quality preschool education, daycare, activity  centres, and after school programmes.
+        <div className={styles.textContainer}>
+            <div className={styles.sectionTitle}>Products</div>
+            <div className={styles.content}>
+                We offer a wide variety of text books and activity books for kids from Pre KG to Std V.
+                Please click below to view our catalogues
+                <div className={styles.productLinks}>
+                    <div className={styles.linkContainer}>
+                        <div className={`${styles.iconContainer} ${styles.book}`}><BookIcon /></div>
+                        <div className={styles.text}>
+                            <div className={styles.title}>School Books</div>
+                            <div className={styles.subTitle}>
+                                Term books with Ques paper, Writing books, Hindi, GK, Computer Science Books, Drawing Books
+                            </div>
+                        </div>
+                        <div className={styles.arrowContainer}>
+                            <MdArrowForwardIos />
+                        </div>
+                    </div>
+                    <div className={styles.linkContainer}>
+                        <div className={`${styles.iconContainer} ${styles.register}`}><RegisterIcon /></div>
+                        <div className={styles.text}>
+                            <div className={styles.title}>Attendance Registers</div>
+                            <div className={styles.subTitle}>
+                                Term books with Ques paper, Writing books, Hindi, GK, Computer Science Books, Drawing Books
+                            </div>
+                        </div>
+                        <div className={styles.arrowContainer}>
+                            <MdArrowForwardIos />
+                        </div>
+                    </div>
+                    <div className={styles.linkContainer}>
+                        <div className={`${styles.iconContainer} ${styles.diary}`}><DiaryIcon /></div>
+                        <div className={styles.text}>
+                            <div className={styles.title}>School Diaries</div>
+                            <div className={styles.subTitle}>
+                                Term books with Ques paper, Writing books, Hindi, GK, Computer Science Books, Drawing Books
+                            </div>
+                        </div>
+                        <div className={styles.arrowContainer}>
+                            <MdArrowForwardIos />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>)
 };
