7bee1bcef7c06a7483f2da4ce4e5c902830de8d4
Barathkumar <barath@192.168.1.101>
2023-05-21 17:50:41 +0200

    feat(): content addition

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -12,7 +12,15 @@ const AboutUs:FC<AboutUsProps> = ({ aboutRef }) => {
         <div className={styles.text}>
             <div className={styles.title}>About Us</div>
             <div className={styles.content}>
-                
+                Associate Prints is a leading educational publisher in Sivakasi. We have vast experience in printing field for about three decades. We are the printers and publishers of KG books, Hindi books, Writing practice books, Computer Science books, Drawing books etc. We are specialist in school assignment diaries and customised compliment diaries. You can contact us for any type of printing work.
+                <div className={styles.innerContent}>Other than books, diaries, we are supplying exam papers, exam pads and printing school function invitations, certificates, pamphlets, brochures and any other printing needs.</div>
+                <div className={styles.innerContent}>We are highly known for the
+                    <ul> 
+                        <li>High quality products</li>
+                        <li>Competitive price</li>
+                        <li>Timely delivery</li>
+                    </ul>
+                </div> 
             </div>
         </div>
     </div>)
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+const priceListLinks = {
+    book: 'https://drive.google.com/file/d/1hBmzGwWcB6onXFFWESNmWKrfQ60Uj5-b/view?usp=sharing',
+    register: 'https://drive.google.com/file/d/1Tg7MjI_laAFKvEnrA3J3l2Rqg_cNyqOV/view?usp=sharing',
+}
+
+export default priceListLinks;
