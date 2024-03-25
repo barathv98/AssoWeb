ce2cd3183092bf3f3b86edb4a26e1e76eb248630
barathv98 <barathkumar.v@fundingsocieties.com>
2024-03-25 19:49:02 +0530

    feat(): text update

--- src/components/MarqueeRibbon/index.tsx
+++ src/components/MarqueeRibbon/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as SuccessIcon } from "../../assets/icons/success.svg";
 import styles from "./styles.module.scss";
 
 const MarqueeRibbon = () => {
-  const text = "2023 - 2024 Academic Year Books, Diaries booking open";
+  const text = "2024 - 2025 Academic Year Books, Diaries booking open";
   return (
     <div className={styles.marquee}>
       <SuccessIcon />
